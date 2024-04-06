279593c37822f3250a0ded2d7e354c61627185e3
TALHAdevelops <160895671+TALHAdevelops@users.noreply.github.com>
2024-04-06 20:04:58 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let currency : any= {
+  USD: 1,
+  EUR: 0.88,
+  PKR: 220,
+  GBP: 0.74,
+  INR: 74.85,
+};
+let userAnswer = await inquirer.prompt([
+  {
+    name: "fromCurrency",
+    type: "list",
+    choices: ["USD", "EUR", "PKR", "GBP", "INR"],
+    message: "Select your from currency :",
+  },
+  {
+    name: "toCurrency",
+    type: "list",
+    choices: ["USD", "EUR", "PKR", "GBP", "INR"],
+    message: "Select your currency to convert:",
+  },
+  {
+    name: "amount",
+    type: "number",
+    message: "Enter the amount to convert:",
+  },
+]);
+let from = currency[userAnswer.fromCurrency];
+let to = currency[userAnswer.toCurrency];
+let amount = userAnswer.amount;
+let result = amount * to / from;
+console.log(result);
+
+
